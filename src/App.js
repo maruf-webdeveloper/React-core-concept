4f54c3bb841a394018e8be3873d72846488729b2
maruf-webdeveloper <maruf.ruet18@gmail.com>
2021-08-14 14:41:28 +0600

    done

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <About name=' Md Maruf Hassan' age='22' job='Web Developer'></About>
+      <About name=' Razu' age='15' job='Student'></About>
+      <About name=' Rasel' age='28' job='cricket Player'></About>
       </header>
     </div>
   );
 }
-
+function About(props){
+  const styleSheet={
+    border:'2px solid red', 
+    margin:'10px',
+    padding: '5px'
+  }
+  return (
+    <div style={styleSheet}>
+      <p >Name:{props.name} </p>
+      <p >Age:{props.age} </p>
+      <p>Profession:{props.job} </p>
+    </div>
+  ) 
+}
 export default App;
